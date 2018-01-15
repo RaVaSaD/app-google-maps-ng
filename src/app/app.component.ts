e9aa80fef1bedb5142953e548dca41ab169fd35e
NENES-PC2\Admin <ravasad@gmail.com>
2018-01-15 21:49:51 +0100

    Initial Commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { MapasService } from "./services/mapas.service";
+import { Marcador } from "./interfaces/marcador.interface";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  //Coordenadas de Vector
+  lat: number = 40.4622576;
+  lng: number = -3.8134384;
+  zoom:number = 16;
+
+  marcadorSel:any = null;
+  draggable:string="1";
+
+  constructor( public _ms:MapasService ){
+    this._ms.cargarMarcadores();
+  }
+
+  clickMapa( evento ){
+
+    let nuevoMarcador:Marcador ={
+      lat: evento.coords.lat,
+      lng: evento.coords.lng,
+      titulo: "Sin titulo",
+      draggable: true
+    }
+
+    this._ms.insertarMarcador( nuevoMarcador );
+
+  }
+
+  clickMarcador(marcador:Marcador, i:number){
+    console.log(marcador, i);
+    this.marcadorSel = marcador;
+
+    if( this.marcadorSel.draggable ){
+      this.draggable = "1";
+    }else{
+      this.draggable = "0";
+    }
+  }
+
+  cambiarDraggable(){
+    console.log(this.draggable);
+    if( this.draggable == "1" ){
+      this.marcadorSel.draggable = true;
+    }else{
+      this.marcadorSel.draggable = false;
+    }
+  }
+
+
+  dragEndMarcador( marcador:Marcador, evento ){
+    // console.log(marcador, evento);
+    let lat = evento.coords.lat;
+    let lng = evento.coords.lng;
+
+    marcador.lat = lat;
+    marcador.lng = lng;
+
+    this._ms.guardarMarcadores();
+
+  }
+
+}
